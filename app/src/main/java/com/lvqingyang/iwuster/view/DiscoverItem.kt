1a5cbee55e4012abc77c655e335d0095a415606f
biloba12345 <2373880422@qq.com>
2018-01-17 21:44:47 +0800

    discover页面

---
+++ app/src/main/java/com/lvqingyang/iwuster/view/DiscoverItem.kt
@@ -0,0 +1,38 @@
+package com.lvqingyang.iwuster.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.lvqingyang.iwuster.R
+import kotlinx.android.synthetic.main.layout_discover_item.view.*
+
+/**
+ * 一句话功能描述
+ * 功能详细描述
+ * @author Lv Qingyang
+ * @see 相关类/方法
+ * @since
+ * @date 2018/1/17
+ * @email biloba12345@gamil.com
+ * @github https://github.com/biloba123
+ * @blog https://biloba123.github.io/
+ */
+class DiscoverItem(context: Context?, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_discover_item, this)
+        if (context!=null && attrs != null) {
+            val arr=context.theme.obtainStyledAttributes(
+                    attrs, R.styleable.DiscoverItem, 0, 0)
+            try {
+                iv_ic.setImageResource(arr.getResourceId(R.styleable.DiscoverItem_icon, R.mipmap.ic_launcher))
+                tv_name.text=arr.getString(R.styleable.DiscoverItem_name)
+            }finally {
+                arr.recycle()
+            }
+        }
+
+    }
+
+}
